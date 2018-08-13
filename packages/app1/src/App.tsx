b861a8adc1cff000f79652f7ad91dca8ac090c8b
dimo89 <dmitri.karasjov@gmail.com>
2018-08-13 11:33:31 +0300

    init app1

---
+++ packages/app1/src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const App1: React.SFC = () => (
+  <div>App1</div>
+);
+
+export default App1;
---
+++ packages/app1/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.tsx',
+  output: {
+    filename: 'app1.js',
+    library: 'app1',
+    libraryTarget: 'umd'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.jsx', '.json'],
+    symlinks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'awesome-typescript-loader',
+        options: {
+          useBabel: true,
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/img/[name].[ext]?[hash]'
+        }
+      },
+    ]
+  },
+  externals: [
+    'react',
+    'react-dom',
+    '@material-ui/core',
+    '@material-ui/icons',
+    'react-router-dom',
+  ]
+};
